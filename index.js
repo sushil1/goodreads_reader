1f764745b83524102835c04136654c38387bfc2f
Sushil L <sushil_samson@hotmail.com>
2017-09-04 00:56:43 +1000

    initial commit

---
+++ index.js
@@ -0,0 +1,19 @@
+import express from 'express'
+import path from 'path'
+
+const app = express()
+
+app.post('/api/auth', (req, res)=> {
+  res.status(400).json({errors: {global: "Invalid credentials"} })
+})
+
+app.get('/*', (req, res) => {
+  res.sendFile(path.join(__dirname, 'index.html'))
+})
+
+
+
+
+const PORT = process.env.PORT || 8080
+
+app.listen(PORT, ()=> console.log(`app running on ${PORT}`))
